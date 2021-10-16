fc3e33ff398c5aa42ca2a95c3da3120ce5e2b844
chenyuan <cy476571@gmail.com>
2021-10-16 08:07:52 +0800

    make bricks disapeared when touched by ball

--- breakout-game.js
+++ breakout-game.js
@@ -9,12 +9,16 @@
   let raf = null
   let isBallRunning = false
   let ifDown = false
+  // let can
 
   const generateBricksNum = 30
+  const generatedBricks = []
+  let firstGenerateBricks = true
+  const disapearedBrickIndexes = []
 
   const ball = {
     x: 50,
-    y: 50,
+    y: 200,
     vx: 7,
     vy: 11,
     radius: 12.5,
@@ -62,6 +66,7 @@
       width: 100,
       height: 20,
       color: 'green',
+      disapearColor: 'transparent',
     }
 
     let xMult = 0
@@ -69,7 +74,12 @@
 
     for (let i = 0; i < num; i++) {
       ctx.beginPath()
-      ctx.strokeStyle = brick.color
+
+      if (disapearedBrickIndexes.includes(i)) {
+        ctx.strokeStyle = brick.disapearColor
+      } else {
+        ctx.strokeStyle = brick.color
+      }
 
       if (xMult * brick.width >= 800) {
         xMult = 0
@@ -77,27 +87,24 @@
       }
 
       ctx.strokeRect(xMult * brick.width, yMult, brick.width, brick.height)
+      if (firstGenerateBricks) {
+        generatedBricks.push({
+          x: xMult * brick.width,
+          y: yMult,
+          width: brick.width,
+          height: brick.height,
+        })
+      }
       xMult += 1
     }
-  }
-
-  function ifCollision(ball, paddle) {
-    const { x, y } = ball
-    let isTouched = false
-
-    if ((x > paddle.x && x < paddle.x + paddle.width)
-        && (y === paddle.y && y >= paddle.y)
-       ) {
-         isTouched = true
-    }
-
-    return isTouched
+    firstGenerateBricks = false
   }
 
   function stopBall() {
     window.cancelAnimationFrame(raf)
     isBallRunning = false
     btn.textContent = 'Start'
+    btn.classList.remove('hide')
   }
 
   function draw() {
@@ -129,12 +136,27 @@
       ball.vx = -ball.vx;
     }
 
+    // Check ball touch with bottom paddle
     if (realY > paddle.y
         && realX > paddle.x
         && (realX < (paddle.x + paddle.width))) {
       ball.vy = -ball.vy
     }
 
+    // Check ball touch top bricks
+    for (let j = 0; j < generatedBricks.length; j++) {
+      const brick = generatedBricks[j]
+      const brickBottomY = brick.y + brick.height
+      if (ball.y <= brickBottomY && ball.x > brick.x && ball.x < (brick.x + brick.width)) {
+        if (!disapearedBrickIndexes.includes(j)) {
+          ball.vy = -ball.vy
+          disapearedBrickIndexes.push(j)
+        }
+      }
+
+      // Add other touch cases
+      // if ((ball.y + ball.height) )
+    }
 
     // // Check if ball && paddle touched
     // if (ifCollision(ball, paddle)) {
@@ -157,12 +179,13 @@
     if (!isBallRunning) {
       if (ifDown) {
         ball.x = 50
-        ball.y = 50
+        ball.y = 200
       }
 
       isBallRunning = true
       raf = window.requestAnimationFrame(draw);
       btn.textContent = 'Stop'
+      btn.classList.add('hide')
     }
   })
 
